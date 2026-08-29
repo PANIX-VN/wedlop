86cd24251c4c462abcc04b46bacba696dea6b39a
unknown <mynameminh22@gmail.com>
2026-08-30 02:13:07 +0700

    Ap dung Bang mau 1 cho Light Mode va Bang mau 2 cho Dark Mode GUI/UX

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        // Palette 1 - Light Mode
+        paletteLight: {
+          bg: '#F5EFE6',
+          card: '#E8DFCA',
+          primary: '#6D94C5',
+          secondary: '#CBDCEB',
+        },
+        // Palette 2 - Dark Mode
+        paletteDark: {
+          accent: '#2F2FE4',
+          primary: '#162E93',
+          card: '#1A1953',
+          bg: '#080616',
+        },
         brand: {
           50: '#f0f9ff',
           100: '#e0f2fe',
